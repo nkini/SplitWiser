443444e57c9434806ac887ac47134ddca3a68e6b
Nikhil Kini <nkini@ucsc.edu>
2017-05-18 00:21:22 -0700

    Don't forget to tip!

--- SplitWiser.py
+++ SplitWiser.py
@@ -2,7 +2,6 @@ import sys
 
 print("Welcome to SplitWiser")
 
-
 def fileinput():
     
     with open(sys.argv[1]) as f:
@@ -19,9 +18,7 @@ def fileinput():
     print("Overview: ")
     print(amt_list)
 
-    tax = float(input("Enter Tax: "))
-    gratuity = float(input("Enter gratuity: "))
-    caculate_splits(amt_list, tax, gratuity)
+    caculate_splits(amt_list)
 
 
 def commandline():
@@ -44,12 +41,14 @@ def commandline():
     print("Overview: ")
     print(amt_list)
 
+    caculate_splits(amt_list)
+
+
+def caculate_splits(amt_list):
     tax = float(input("Enter Tax: "))
-    gratuity = float(input("Enter gratuity: "))
-    caculate_splits(amt_list, tax, gratuity)
+    gratuity = float(input("Enter Gratuity: "))
+    tip = float(input("Enter Tip Amount: "))
 
-
-def caculate_splits(amt_list, tax, gratuity):
     person_total = {patron:sum(amts) for patron,amts in amt_list.items()}
     total_total = sum(person_total.values())
     person_total_and_proportion = {patron:(total,total/total_total) for patron,total in person_total.items()}
@@ -59,7 +58,7 @@ def caculate_splits(amt_list, tax, gratuity):
     for person, t_p in person_total_and_proportion.items():
         total = t_p[0]
         prop  = t_p[1]
-        final_amt = total + prop*tax + prop*gratuity
+        final_amt = total + prop*tax + prop*gratuity + prop*tip
         tally += final_amt
         print("{} pays {}".format(person,final_amt))
 
